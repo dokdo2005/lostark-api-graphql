15f9280ce9ecea98f10283a4d33a311ddb251f2f
Gary Lee <dokdo2005@gmail.com>
2023-02-15 19:59:16 +0900

    chore: graphql code-first

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { ApolloDriver } from '@nestjs/apollo';
     }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
     }),
     NewsModule,
   ],
